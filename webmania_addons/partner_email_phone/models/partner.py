b5c317d416ca417f4047677040d551515c26d871
Driss El mouedden <d.elmouedden@gmail.com>
2024-09-28 12:48:37 +0100

    Add filed mobile,street,city,county_id,zip

--- webmania_addons/partner_email_phone/models/partner.py
+++ webmania_addons/partner_email_phone/models/partner.py
@@ -1,4 +1,3 @@
-
 from odoo import models, fields
 
 class ResPartner(models.Model):
@@ -6,3 +5,8 @@ class ResPartner(models.Model):
 
     phone = fields.Char(required=True)
     email = fields.Char(required=True)
+    mobile = fields.Char(required=True)
+    street = fields.Char(required=True)
+    city = fields.Char(required=True)
+    country_id = fields.Many2one('res.country', string='Country', required=True)
+    zip = fields.Char(required=True)
