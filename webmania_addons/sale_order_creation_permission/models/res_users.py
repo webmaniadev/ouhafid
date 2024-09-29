d4424e47df1a0b8b5d2714e74da48b1862eb0983
Driss El mouedden <d.elmouedden@gmail.com>
2024-09-29 12:22:09 +0100

    update

--- webmania_addons/sale_order_creation_permission/models/res_users.py
+++ webmania_addons/sale_order_creation_permission/models/res_users.py
@@ -4,4 +4,6 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    allow_sale_order_creation = fields.Boolean(string="Allow Sale Order Creation", default=True)
+    def action_check_sale_order_creation(self):
+        # Vérifiez si l'utilisateur est dans un groupe autorisé
+        return self.env.user.has_group('base.group_sale_manager')
