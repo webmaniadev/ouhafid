6e2f0de86e54c4c96a106707f4fb1622d79af561
Driss El mouedden <d.elmouedden@gmail.com>
2024-10-23 09:54:02 +0100

    add pos_discount

--- extra_addons/pw_pos_change_logo/models/pos_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, models, fields, _
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    pw_change_logo = fields.Boolean('Change Logo')
-    pw_pos_logo = fields.Binary(string="POS Logo")
