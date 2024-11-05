dc0ca1b7e914ade7b4f59b1a2f97d8c84453510a
Driss El mouedden <d.elmouedden@gmail.com>
2024-11-05 12:07:00 +0100

    add module hide tax

---
+++ webmania_addons/hide_taxes/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide Taxes",
+    'description': """
+        This module is HIde the Taxes from Ticket POS
+    """,
+    'author': "Webmania",
+    'website': "http://www.webmania.ma",
+    'category': 'POS',
+    'version': '0.1',
+    'depends': ['base' , 'point_of_sale'],
+     'qweb': ['static/src/xml/pos.xml'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
